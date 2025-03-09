9557f94c0f980bd244c341650fa2c38664fd4a1a
Naominm <naomimbugua536@gmail.com>
2025-03-09 23:57:14 +0300

    Add Animation to the Featured destination

--- src/pages/Home/FeaturedDestination.jsx
+++ src/pages/Home/FeaturedDestination.jsx
@@ -1,8 +1,10 @@
-import { FaHeart } from "react-icons/fa";
+import { FaHeart,FaCircle } from "react-icons/fa";
+
 import FeaturedImage1 from "../../assets/mt-kenya.png";
 import FeaturedImage2 from "../../assets/lake-nakuru.jpg";
 import FeaturedImage3 from "../../assets/coastal-kenya.jpg";
 import FeaturedImage4 from "../../assets/Nairobi-national-park.jpg";
+
 
 
 import "./FeaturedDestination.css"
@@ -11,12 +13,25 @@ function FeaturedDestination(){
         <div className="main-feature-container">
             <h3>Featured Destination</h3>
         <div className="featured-destination-container">
+            <div className="scroll-wrapper">
        <FeaturedDestinationElement Image={FeaturedImage1} Text="Mt Kenya"Icon={<FaHeart />}/>
        <FeaturedDestinationElement Image={FeaturedImage2} Text="Lake Nakuru"Icon={<FaHeart />}/>
        <FeaturedDestinationElement Image={FeaturedImage3} Text="Coastal Kenya"Icon={<FaHeart />}/>
        <FeaturedDestinationElement Image={FeaturedImage4} Text="Nairobi N Park"Icon={<FaHeart />}/>
-   
+       
+       <FeaturedDestinationElement Image={FeaturedImage1} Text="Mt Kenya"Icon={<FaHeart />}/>
+       <FeaturedDestinationElement Image={FeaturedImage2} Text="Lake Nakuru"Icon={<FaHeart />}/>
+       <FeaturedDestinationElement Image={FeaturedImage3} Text="Coastal Kenya"Icon={<FaHeart />}/>
+       <FeaturedDestinationElement Image={FeaturedImage4} Text="Nairobi N Park"Icon={<FaHeart />}/>
+       </div>
         </div>
+        {/* <div className="feature-active-class">
+
+      <FeatureActiveElement FeatureActiveCircleIcon={<FaCircle/>}/>
+      <FeatureActiveElement FeatureActiveCircleIcon={<FaCircle/>}/>
+      <FeatureActiveElement FeatureActiveCircleIcon={<FaCircle/>}/>
+      <FeatureActiveElement FeatureActiveCircleIcon={<FaCircle/>}/> 
+        </div> */}
         </div>
     )
 }
@@ -46,6 +61,14 @@ function FeaturedDestinationElement({Image,Text, Icon}){
          </div>
         </div>
         </div>
+    )
+}
+
+function FeatureActiveElement({FeatureActiveCircleIcon}){
+    return(
+     <div className="feature-active">
+{FeatureActiveCircleIcon}
+                </div>
     )
 }
 
